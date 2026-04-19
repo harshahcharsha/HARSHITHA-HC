175ffa58945bfcc862e5363deff9bf828e4f4a18
harshahcharsha <harshahcharsha@gmail.com>
2026-04-19 23:44:39 +0530

    Update Mysql_Query.java

--- src/main/java/com/mysql/Mysql_Query.java
+++ src/main/java/com/mysql/Mysql_Query.java
@@ -1,5 +1,7 @@
 package com.mysql;
+/*  Read all the existing records from the table coffee which is from the database test and query coffee name starting with ‘D’   in the table.
 
+*/
 import java.sql.*;
 import java.util.Properties;
 
